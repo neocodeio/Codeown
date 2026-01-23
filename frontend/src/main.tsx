e382c92bb888bef80f8a4380898415dfe7362cfe
andmeen <ameenfaisal8099@gmail.com>
2026-01-23 19:06:39 +0300

    speed

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,4 +1,5 @@
 import ReactDOM from "react-dom/client";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { ClerkWrapper } from "./lib/ClerkWrapper";
@@ -8,6 +9,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <ClerkWrapper>
       <App />
+      <SpeedInsights />
     </ClerkWrapper>
   </BrowserRouter>
 );
