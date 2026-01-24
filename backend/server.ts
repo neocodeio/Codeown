ac53f650d290cd5eb6b82f379f0220c736043928
andmeen <ameenfaisal8099@gmail.com>
2026-01-24 19:33:33 +0300

    port number

--- backend/server.ts
+++ backend/server.ts
@@ -2,13 +2,13 @@ import "dotenv/config";
 import app from "./app.js";
 import type { Request, Response } from "express";
 
-const PORT = process.env.PORT || 5000;
+const PORT = Number(process.env.PORT) || 5000;
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Backend is running");
 });
 
-app.listen(Number(PORT), "0.0.0.0", () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Backend running on port ${PORT}`);
   console.log("FRONTEND_URL:", process.env.FRONTEND_URL);
   console.log("SUPABASE_URL", process.env.SUPABASE_URL);
