61a0ebab45d40943ad9290a21c159ae016830d13
andmeen <ameenfaisal8099@gmail.com>
2026-01-27 13:36:32 +0300

    adding posthog

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,13 +5,21 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { ClerkWrapper } from "./lib/ClerkWrapper";
 import "@fortawesome/fontawesome-svg-core/styles.css";
+import { PostHogProvider } from 'posthog-js/react'
+
+const options = {
+  api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+  defaults: '2025-11-30',
+} as const
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <ClerkWrapper>
-      <App />
-      <SpeedInsights />
-      <Analytics />
+      <PostHogProvider apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY} options={options}>
+        <App />
+        <SpeedInsights />
+        <Analytics />
+      </PostHogProvider>
     </ClerkWrapper>
   </BrowserRouter>
 );
