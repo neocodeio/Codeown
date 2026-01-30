5335c2ca745cd54c64848a6cefe1199645e70454
andmeen <ameenfaisal8099@gmail.com>
2026-01-30 23:28:42 +0300

    adding Sentry

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,24 @@
-import ReactDOM from "react-dom/client";
+import ReactDOM, { createRoot } from "react-dom/client";
 import { SpeedInsights } from "@vercel/speed-insights/react";
 import { Analytics } from "@vercel/analytics/react"
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { ClerkWrapper } from "./lib/ClerkWrapper";
 import "@fortawesome/fontawesome-svg-core/styles.css";
+import * as Sentry from "@sentry/react";
 
+Sentry.init({
+  dsn: "https://542b494407c8004af3549ac6080c9239@o4510801216995328.ingest.us.sentry.io/4510801218240512",
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  sendDefaultPii: true
+});
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+const container = document.getElementById("root");
+const root = createRoot(container!);
+
+root.render(
+  <Sentry.ErrorBoundary fallback={<p>Something went wrong.</p>}>
   <BrowserRouter>
     <ClerkWrapper>
       <App />
@@ -15,4 +26,5 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
       <Analytics />
     </ClerkWrapper>
   </BrowserRouter>
+</Sentry.ErrorBoundary>
 );
