a80b6228b90b125d4c40d64a7f315b4a20cd1e9d
andmeen <ameenfaisal8099@gmail.com>
2026-01-31 20:52:55 +0300

    undo sentry

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,33 +5,11 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { ClerkWrapper } from "./lib/ClerkWrapper";
 import "@fortawesome/fontawesome-svg-core/styles.css";
-import * as Sentry from "@sentry/react";
-
-Sentry.init({
-  dsn: "https://542b494407c8004af3549ac6080c9239@o4510801216995328.ingest.us.sentry.io/4510801218240512",
-  // Setting this option to true will send default PII data to Sentry.
-  // For example, automatic IP address collection on events
-  sendDefaultPii: true
-});
 
 const container = document.getElementById("root");
 const root = createRoot(container!);
 
-// Add this button component to your app to test Sentry's error tracking
-function ErrorButton() {
-  return (
-    <button
-      onClick={() => {
-        throw new Error('This is your first error!');
-      }}
-    >
-      Break the world
-    </button>
-  );
-}
-
 root.render(
-  <Sentry.ErrorBoundary fallback={<p>Something went wrong.</p>}>
     <BrowserRouter>
       <ClerkWrapper>
         <App />
@@ -39,6 +17,4 @@ root.render(
         <Analytics />
       </ClerkWrapper>
     </BrowserRouter>
-    <ErrorButton />
-  </Sentry.ErrorBoundary>
 );
