1a9a64e3cd0acec33819ae5e7676975ee021266a
andmeen <ameenfaisal8099@gmail.com>
2026-03-27 15:23:53 +0300

    fix the margin

--- frontend/src/pages/Feed.tsx
+++ frontend/src/pages/Feed.tsx
@@ -157,6 +157,7 @@ export default function Feed() {
                             display: "flex",
                             alignItems: "center",
                             height: "56px",
+                            marginBottom: "-8px",
                             width: "100%",
                             position: "relative",
                             justifyContent: "center",
